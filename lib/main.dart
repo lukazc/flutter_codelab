ed12ac950fc86ff828a1f24ce9dbf7875ebe9fd0
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2024-12-02 10:08:44 +0100

    Update color scheme to deep orange and change text styling

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
           //
           // This works for code too, not just values: Most code changes can be
           // tested with just a hot reload.
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
           useMaterial3: true,
         ),
         home: MyHomePage(),
@@ -84,10 +84,16 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textStyle = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+  
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase),
+        child: Text(pair.asLowerCase, style: textStyle),
       ),
     );
   }

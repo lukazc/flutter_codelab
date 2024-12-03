a6958dedfd45e2716c01ed7011ee96ac442d9f0d
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2024-12-03 08:14:37 +0100

    Refactor HomePage layout to use LayoutBuilder for responsive NavigationRail

--- lib/main.dart
+++ lib/main.dart
@@ -82,38 +82,42 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.home),
-                  label: Text('Home'),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth > 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedPageIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedPageIndex = value;
+                    });
+                  },
                 ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.favorite),
-                  label: Text('Favorites'),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: getPage(),
                 ),
-              ],
-              selectedIndex: selectedPageIndex,
-              onDestinationSelected: (value) {
-                setState(() {
-                  selectedPageIndex = value;
-                });
-              },
-            ),
+              ),
+            ],
           ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: getPage(),
-            ),
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
